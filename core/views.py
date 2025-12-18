455f560352dfcc6f7fcd3addd7c3c67a14be9454
Rodolfo_Gomez <ro.gohe08@gmail.com>
2025-12-18 15:17:17 -0300

    Diseño Login: Configuración inicial

--- core/views.py
+++ core/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def home(request):
-    # Renderizamos la plantilla específica del home
+    # Solo una línea return. La anterior estaba bloqueando.
     return render(request, 'home.html')
+
+def registro(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Cuenta creada para {username}! Ya puedes iniciar sesión.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registro.html', {'form': form})
