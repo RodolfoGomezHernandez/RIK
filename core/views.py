53694db7a429a9187dde192726fd6ea318580998
Rodolfo_Gomez <ro.gohe08@gmail.com>
2025-12-18 14:41:41 -0300

    Diseño home: Se crean las views, templates home

--- core/views.py
+++ core/views.py
@@ -3,3 +3,4 @@ from django.shortcuts import render
 def home(request):
     # Renderizamos base.html temporalmente para probar el diseño
     return render(request, 'base.html')
+    return render(request, 'home.html')
