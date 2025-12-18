f1d933776df673182253e3987a5dffddafe5a8b3
Rodolfo_Gomez <ro.gohe08@gmail.com>
2025-12-17 21:35:42 -0300

    Diseño Base: Integración de navbar

--- eventos_rik_project/settings.py
+++ eventos_rik_project/settings.py
@@ -68,7 +68,10 @@ TEMPLATES = [
 ]
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 WSGI_APPLICATION = 'eventos_rik_project.wsgi.application'
 
