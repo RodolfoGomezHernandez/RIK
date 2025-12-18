22f75211da3322977216c46fa846eb51b760282e
Rodolfo_Gomez <ro.gohe08@gmail.com>
2025-12-18 15:27:03 -0300

    Fix: Corrección de importación de auth_views

--- eventos_rik_project/urls.py
+++ eventos_rik_project/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core.views import home
-from core import views 
+from django.contrib.auth import views as auth_views
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
